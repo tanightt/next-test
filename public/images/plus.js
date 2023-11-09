c09ebc3f028787ce474cb802438a1a3b3feefcaa
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-11-09 17:34:45 +0200

    created project

---
+++ public/images/plus.js
@@ -0,0 +1,29 @@
+const Plus = () => {
+  return (
+    <>
+      <svg
+        width="15"
+        height="14"
+        viewBox="0 0 15 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7.65039 2.33594V11.41"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M3.11328 6.87109H12.1874"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+export default Plus;
---
+++ public/images/search.js
@@ -0,0 +1,30 @@
+const Search = () => {
+  return (
+    <>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M9.36337 14.4553C12.175 14.4553 14.4543 12.176 14.4543 9.36435C14.4543 6.55272 12.175 4.27344 9.36337 4.27344C6.55174 4.27344 4.27246 6.55272 4.27246 9.36435C4.27246 12.176 6.55174 14.4553 9.36337 14.4553Z"
+          stroke="#9B9D9F"
+          strokeWidth="1.6"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M15.7272 15.7252L12.959 12.957"
+          stroke="#9B9D9F"
+          strokeWidth="1.6"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default Search;
